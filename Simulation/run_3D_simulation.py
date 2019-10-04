200870f52aa36bfd70d6cbb33a6e950789738ae9
bobzwik <john.bobzwik@gmail.com>
2019-10-04 16:13:25 -0400

    Added position error plot

--- Simulation/run_3D_simulation.py
+++ Simulation/run_3D_simulation.py
@@ -57,9 +57,9 @@ def main():
     #                                  3: minimum velocity          4: minimum accel,           5: minimum jerk,           6: minimum snap
     #                                  7: minimum accel_stop        8: minimum jerk_stop        9: minimum snap_stop
     #                                 10: minimum jerk_full_stop   11: minimum snap_full_stop
-    trajSelect[0] = 10          
+    trajSelect[0] = 4          
     # Select Yaw Trajectory Type      (0: none                      1: yaw_waypoint_timed,      2: yaw_waypoint_interp     3: follow          4: zero)
-    trajSelect[1] = 4           
+    trajSelect[1] = 3           
     # Select if waypoint time is used, or if average speed is used to calculate waypoint time   (0: waypoint time,   1: average speed)
     trajSelect[2] = 0           
     print("Control type: {}".format(ctrlType))
