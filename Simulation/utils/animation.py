c3f349bf06e3ceb9903293c2e66473844a5514e2
John <john.bobzwik@gmail.com>
2019-10-05 12:29:17 -0400

    resize gifs

--- Simulation/utils/animation.py
+++ Simulation/utils/animation.py
@@ -166,7 +166,7 @@ def sameAxisAnimation(t_all, waypoints, pos_all, quat_all, sDes_tr_all, Ts, para
     line_ani = animation.FuncAnimation(fig, updateLines, init_func=ini_plot, frames=len(t_all[0:-2:numFrames]), interval=(Ts*1000*numFrames), blit=False)
     
     if (ifsave):
-        line_ani.save('Gifs/animation_{0:.0f}_{1:.0f}.gif'.format(xyzType,yawType), dpi=80, writer='imagemagick', fps=25)
+        line_ani.save('Gifs/Raw/animation_{0:.0f}_{1:.0f}.gif'.format(xyzType,yawType), dpi=80, writer='imagemagick', fps=25)
         
     plt.show()
     return line_ani
